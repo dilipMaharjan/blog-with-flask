2925c7d609cce98aed504b2df801e662cc2e7383
crazdrms <dilipmaharjann@gmail.com>
2018-09-06 11:00:10 -0400

    me route

--- basics.py
+++ basics.py
@@ -8,6 +8,11 @@ def hello():
     return "Hello Flask"
 
 
+@app.route("/me")
+def me():
+    return '<h1>This is me </h1>'
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port='5000')
