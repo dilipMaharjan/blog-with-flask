f3909e1842a14cf54b81315383b04dc5f5e30eec
crazdrms <dilipmaharjann@gmail.com>
2018-09-06 11:32:46 -0400

    templates and layouts

--- basics.py
+++ basics.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Dilip',
+        'title': 'blog 1',
+        'content': 'This is a blog post'
+    },
+    {
+        'author': 'John',
+        'title': 'blog 2',
+        'content': 'This is a blog post'
+    },
+    {
+        'author': 'Matthew',
+        'title': 'blog 3',
+        'content': 'This is a blog post'
+    }
+]
 
 
 @app.route("/")
@@ -8,9 +26,19 @@ def hello():
     return "Hello Flask"
 
 
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts, title="home")
+
+
 @app.route("/me")
 def me():
     return '<h1>This is me </h1>'
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='about')
 
 
 if __name__ == '__main__':
